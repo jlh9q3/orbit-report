9c97c95bff3d7c0711e9ea2946b4b2a3725407bb
Jessica Hudson <jess@Jessicas-MacBook-Air.local>
2020-08-08 15:32:45 -0500

    completed steps 1-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'orbit-report';
+  sourceList: Satellite[];
+
+  constructor() {
+    this.sourceList = [];
+    
+  }
 }
